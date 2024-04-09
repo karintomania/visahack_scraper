b0f5f692baa1fd801097a35c6ffe42aeb82816c1
karintomania <19652340+karintomania@users.noreply.github.com>
2024-04-09 15:59:56 +0100

    optimise read_html by making web driver singleton

--- scraper/read_html.py
+++ scraper/read_html.py
@@ -3,13 +3,32 @@ from selenium.webdriver.chrome.service import Service
 import re
 import datetime
 from scraper.option import get_options
+import atexit
 
+class WebDriver:
+    _instance = None
+
+    @classmethod
+    def get_instance(cls):
+        if cls._instance is None:
+            options = get_options()
+            s = Service("/usr/bin/chromedriver")
+            cls._instance = webdriver.Chrome(service=s, options=options)
+            atexit.register(cls._close_driver)
+        return cls._instance
+
+    @classmethod
+    def _close_driver(cls):
+        if cls._instance:
+            cls._instance.quit()
+            cls._instance = None
 
 def read_html(url: str) -> str:
     options = get_options()
     s = Service("/usr/bin/chromedriver")
-    with webdriver.Chrome(service=s, options=options) as driver:
-        driver.get(url)
-        html_source = driver.page_source
+
+    driver = WebDriver.get_instance()
+    driver.get(url)
+    html_source = driver.page_source
 
     return html_source
