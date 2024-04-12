3b178cf50d1de8c4fbeb74bffef18dd124a985af
karintomania <19652340+karintomania@users.noreply.github.com>
2024-04-12 10:45:58 +0100

    make env var importing type-safe

--- const/common.py
+++ const/common.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import sys
 import os
 
 
@@ -7,11 +6,11 @@ PROJECT_PATH = str(Path(__file__).resolve().parent.parent)
 # LOG_PATH = PROJECT_PATH + "/logs"
 
 # Days to deactivate the job
-JOB_EXPIRED_DAYS = os.getenv("JOB_EXPIRED_DAYS", 30)
+JOB_EXPIRED_DAYS = int(os.getenv("JOB_EXPIRED_DAYS", 30))
 # Days to remove the job from DB
-JOB_REMOVAL_DAYS = os.getenv("JOB_REMOVAL_DAYS", 60)
+JOB_REMOVAL_DAYS = int(os.getenv("JOB_REMOVAL_DAYS", 60))
 
-SLEEP_BETWEEN_URL = os.getenv("SLEEP_BETWEEN_URL", 10)
-SLEEP_BETWEEN_DETAILS = os.getenv("SLEEP_BETWEEN_DETAILS", 5)
+SLEEP_BETWEEN_URL = int(os.getenv("SLEEP_BETWEEN_URL", 10))
+SLEEP_BETWEEN_DETAILS = int(os.getenv("SLEEP_BETWEEN_DETAILS", 5))
 
-SCRAPING_RETRY_ON_ERROR = os.getenv("SCRAPING_RETRY_ON_ERROR", 2)
+SCRAPING_RETRY_ON_ERROR = int(os.getenv("SCRAPING_RETRY_ON_ERROR", 2))
