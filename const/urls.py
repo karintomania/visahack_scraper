6f02998affef7336a9c7b174e57bef1ab1238721
karintomania <19652340+karintomania@users.noreply.github.com>
2024-04-12 10:56:29 +0100

    remove queries from urls module

--- const/urls.py
+++ const/urls.py
@@ -7,12 +7,6 @@ class Websites(Enum):
     REED = "reed"
 
 
-queries = {
-    Websites.INDEED: {
-        Countries.GB: "https://uk.indeed.com/jobs?q=software+engineer+%22UK+visa+sponsorship%22&l=United+Kingdom&sort=date&start={}",
-        Countries.US: "https://www.indeed.com/jobs?q=software+engineer+%22Visa+sponsorship%22&l=United+States&sort=date&start={}",
-    },
-}
 link_prefixes = {
     Websites.INDEED: {
         Countries.GB: "https://uk.indeed.com{}",
