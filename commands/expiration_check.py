9ee8dd34bc1f9ea218459d726095dc56a1643bb8
karintomania <19652340+karintomania@users.noreply.github.com>
2024-05-09 12:23:57 +0100

    refactor expiration check command

--- commands/expiration_check.py
+++ commands/expiration_check.py
@@ -7,6 +7,24 @@ from expiration_checker.indeed_expiration_checker import IndeedExpirationChecker
 from models.job import Job
 
 
+def is_job_expired(checker: ExpirationChecker, job: Job) -> bool:
+    checker_name = type(checker).__name__
+    try:
+        is_url_expired = checker.is_url_expired(job.url)
+
+        if is_url_expired:
+            logger.info(f"Expired job id: {job.id}")
+
+        job.active = not is_url_expired
+        job.save()
+        return is_url_expired
+
+    except Exception as e:
+        logger.error(f"Error on checking expiration on: {checker_name}, job: {job.id}")
+        logger.exception(e)
+        return False
+
+
 def check_expiration(checker: ExpirationChecker):
     checker_name = type(checker).__name__
     logger.info(f"start expiration check: {checker_name}")
@@ -16,20 +34,11 @@ def check_expiration(checker: ExpirationChecker):
 
     for job in jobs_to_check:
         sleep(SLEEP_BETWEEN_URL)
-        try:
-            is_url_expired = checker.is_url_expired(job.url)
 
-            if is_url_expired:
-                logger.info(f"Expired job id: {job.id}")
-                job.active = False
-                job.save()
-                expired_count += 1
+        result = is_job_expired(checker, job)
 
-        except Exception as e:
-            logger.error(
-                f"Error on checking expiration on: {checker_name}, job: {job.id}"
-            )
-            logger.exception(e)
+        if result:
+            expired_count += 1
 
     logger.info(
         f"finish expiration check: {checker_name}, {expired_count} jobs are expired."
